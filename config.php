50038cc34e8b947d507b371e858b34c289916d02
= <=>
2023-10-21 15:25:33 -0300

    removi comentários desnecessários sobre ENVIRONMENT no config.php

--- config.php
+++ config.php
@@ -9,22 +9,6 @@ $dotenv->load(__DIR__ . '/.env');
 
 define('BASE_URL', 'http://localhost/sistema-gestao-php/');
 
-// $db_config = array();
-
-// if (ENVIRONMENT === 'development') {
-//     define('BASE_URL', 'http://localhost/sistema-gestao-php/');
-//     $db_config['dbname'] = 'estoque';
-//     $db_config['host'] = 'localhost';
-//     $db_config['dbuser'] = 'root';
-//     $db_config['dbpass'] = '';
-// } else if (ENVIRONMENT === 'production') {
-//     define('BASE_URL', 'http://localhost/sistema-gestao-php/');
-//     $db_config['dbname'] = 'estoque';
-//     $db_config['host'] = 'localhost';
-//     $db_config['dbuser'] = 'root';
-//     $db_config['dbpass'] = '';
-// }
-
 global $db;
 
 try {
