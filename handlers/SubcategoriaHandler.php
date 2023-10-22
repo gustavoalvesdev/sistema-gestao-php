b3e60e16081b1212b2de4a82a7eba5698133e5a8
= <=>
2023-10-22 18:02:43 -0300

    documentação do arquivos Handlers_SubcategoriaHAndler.php

--- handlers/SubcategoriaHandler.php
+++ handlers/SubcategoriaHandler.php
@@ -1,5 +1,17 @@
 <?php 
 
+/**
+ * Imprime um JSON para obter subcategorias de uma categoria específica.
+ * 
+ * Esta página recebe uma solicitação POST contendo uma categoria e retorna as
+ * subcategorias correspondentes em formato JSON.
+ * 
+ * @category    Categoria do Produto
+ * @package     Handlers_SucategoriaHandler
+ * @author      Gustavo Alves da Silva
+ * @version     1.0
+ */
+
 require '../config.php';
 require '../vendor/autoload.php';
 
@@ -7,8 +19,5 @@ use Models\Subcategory;
 
 $objSubcategories = new Subcategory();
 
-echo json_encode(
-    $objSubcategories->getSubcategories(
-        $_POST['category']
-    )
-);
+// Obtém as subcategorias para a categoria fornecida via POST
+echo json_encode($objSubcategories->getSubcategories($_POST['category']));
