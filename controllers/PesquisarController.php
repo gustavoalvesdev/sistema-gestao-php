dc2cff83c66d95bacd30a12d062adec3bf42d19c
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-06-07 23:07:42 -0300

    PesquisarController

---
+++ controllers/PesquisarController.php
@@ -0,0 +1,35 @@
+<?php 
+
+namespace Controllers;
+
+use BancoDeDados\BancoDeDadosMySQL;
+use Core\Controller;
+use DAO\UsuarioDAO;
+
+class PesquisarController extends Controller 
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $usuario_dao = new UsuarioDAO();
+        $usuario_dao::obterConexao(new BancoDeDadosMySQL);
+
+        if (! $usuario_dao->verificarLogin($this->usuario)) {
+            header('Location: '.BASE_URL.'login');
+            exit;
+        }
+
+        $this->loadView('template_parts/header', $this->dados);
+    }
+
+    public function index()
+    {
+       echo 'PESQUISANDO!';
+    }
+
+    public function __destruct()
+    {
+        $this->loadView('template_parts/footer');
+    }
+}
