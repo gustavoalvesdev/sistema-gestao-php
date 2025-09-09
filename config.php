8215844bb8d632ccb029d586d8be2a958b96c2d4
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-09-09 14:04:59 -0300

    fix: atualiza BASE_URL para development e production

--- config.php
+++ config.php
@@ -5,13 +5,13 @@ require_once 'environment.php';
 $db_config = array();
 
 if (ENVIRONMENT === 'development') {
-    define('BASE_URL', 'http://localhost/estoque/');
+    define('BASE_URL', 'http://localhost/sistema-gestado-php/');
     $db_config['dbname'] = 'estoque';
     $db_config['host'] = 'localhost';
     $db_config['dbuser'] = 'root';
     $db_config['dbpass'] = '';
 } else if (ENVIRONMENT === 'production') {
-    define('BASE_URL', 'http://localhost/estoque/');
+    define('BASE_URL', 'https://seusistema.com.br/');
     $db_config['dbname'] = 'estoque';
     $db_config['host'] = 'localhost';
     $db_config['dbuser'] = 'root';
