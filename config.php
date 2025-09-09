b18326f3ce898825effdd8a8c6bef70025b1bd0e
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-09-09 14:08:11 -0300

    fix: corrige typo de sistema-gestado-php para sistema-gestao-php no BASE_URL de development no config

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ require_once 'environment.php';
 $db_config = array();
 
 if (ENVIRONMENT === 'development') {
-    define('BASE_URL', 'http://localhost/sistema-gestado-php/');
+    define('BASE_URL', 'http://localhost/sistema-gestao-php/');
     $db_config['dbname'] = 'estoque';
     $db_config['host'] = 'localhost';
     $db_config['dbuser'] = 'root';
